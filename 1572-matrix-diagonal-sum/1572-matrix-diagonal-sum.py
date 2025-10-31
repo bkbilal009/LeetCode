fbbaa5b0f7880607cd415ecf3514e3be496a50fd
Muhammad Bilal <bkbilalbluch@gmail.com>
2025-10-31 19:51:42 +0500

    Time: 0 ms (100%), Space: 18.3 MB (19.62%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        total = 0
+        for i in range(n):
+            total += mat[i][i]           # primary diagonal
+            total += mat[i][n - 1 - i]   # secondary diagonal
+        
+        # if matrix size is odd -> subtract middle element once
+        if n % 2 == 1:
+            total -= mat[n // 2][n // 2]
+        
+        return total
+
