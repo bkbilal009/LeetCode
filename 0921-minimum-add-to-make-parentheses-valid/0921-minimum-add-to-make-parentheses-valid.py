e86604b469b2d05b96c2db7736bb97be8908b805
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-01-02 20:57:16 -0800

    Time: 1 ms (20.29%), Space: 17.3 MB (87.71%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        count = 0
+
+        for c in s:
+            if c == "(":
+                stack.append("(")
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    count += 1
+                
+        count += len(stack)
+        return count
