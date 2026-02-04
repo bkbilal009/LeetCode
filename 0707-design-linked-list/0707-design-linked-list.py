3ca87edb9f9c6cfbeadc5a8ed475d817dea7bd9d
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-02-04 21:38:03 +0500

    Time: 53 ms (48.85%), Space: 20.1 MB (44.62%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__ (self,val=0):
+        self.val = val
+        self.next = None
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = Node(0)
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+
+        curr = self.head.next
+        
+        for _ in range(index):
+            curr = curr.next
+
+        return curr.val
+
+    def addAtHead(self, val: int) -> None:
+        node = Node(val)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+
+        curr.next = Node(val)
+        self.size += 1
+
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.size:
+            return 
+        
+        if index < 0:
+            index = 0
+
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+
+        node = Node(val)
+        node.next = curr.next
+        curr.next = node
+        self.size += 1
+
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+
+        curr.next = curr.next.next
+        self.size -= 1
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
