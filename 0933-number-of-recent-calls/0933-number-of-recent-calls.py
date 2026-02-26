5cce46a4cc9fba04d021e4dacee30c541e4acf5b
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-02-26 21:37:22 +0500

    Time: 51 ms (32.42%), Space: 24.6 MB (43.84%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,15 +1,23 @@
 class RecentCounter:
 
-    def __init__(self):
-        self.time = collections.deque()
+   def __init__(self):
+       self.time = collections.deque()
+       # self.time = []
 
-    def ping(self, t: int) -> int:
-        self.time.append(t)
-        while self.time[0] < t - 3000:
-            self.time.popleft()
-        return len(self.time)
-
-
+   def ping(self, t: int) -> int:
+       # out = 1 # include the recent t
+       # for i in self.time[::-1]:
+       #     if i < t - 3000:
+       #         break
+       #     else:
+       #         out += 1
+       
+       # self.time.append(t)
+       # return out 
+       self.time.append(t)
+       while self.time[0] < t - 3000:
+           self.time.popleft()
+       return len(self.time)
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
