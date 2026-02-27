9dec04133acc927f4392b65608ed92912fab45b3
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-02-27 21:40:45 +0500

    Time: 26 ms (91.23%), Space: 24.6 MB (44.59%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -18,6 +18,7 @@ class RecentCounter:
        while self.time[0] < t - 3000:
            self.time.popleft()
        return len(self.time)
+
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
