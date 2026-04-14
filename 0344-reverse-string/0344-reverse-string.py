f12229336d66403026a259e0fbe3502eefae8482
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-04-14 10:07:07 +0500

    Time: 3 ms (48.06%), Space: 23.4 MB (85.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,10 +5,11 @@ class Solution:
         """
         left = 0
         right = len(s) - 1
+        
 
         while left < right:
-            temp = s[left]
-            s[left] = s[right]
-            s[right] = temp
+            # Swap characters 
+            s[left],s[right] = s[right],s[left]
+
             left += 1
             right -= 1
