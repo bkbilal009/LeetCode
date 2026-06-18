213500751953e43feef08288f7e87bea28f7d8d5
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-06-18 19:56:32 +0500

    Time: 0 ms (100%), Space: 19.7 MB (12.42%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
@@ -0,0 +1,9 @@
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        hour %= 12
+
+        hour_angle = hour * 30 + minutes * 0.5 
+        minute_angle = minutes * 6 
+        diff = abs(hour_angle - minute_angle)
+
+        return min(diff, 360 - diff)
